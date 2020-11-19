1be7fbfbf5890059aacfabbf41d189e6feb9881e
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-11-18 22:41:21 -0300

    Exploração inicial do dataset

--- src/main.R
+++ src/main.R
@@ -10,3 +10,11 @@ data
 
 #Limpeza de dados
 sum(is.na(data)) #Nenhum dado com atributo ausente
+data <-data[,-1]
+
+#Exploração estatistica
+comDoenca = sum(data$chd == 1) #160 tiveram doença na coronaria
+semDoenca = sum(data$chd == 0) #360 Não tiveram doença na coronaria
+
+#Faixa de idade do dataset
+ggplot(data, aes(x=age)) +  geom_histogram(color="black", fill="white")+ geom_density(alpha=.2, fill="#FF6666") 
