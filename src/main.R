568dd352a955101c6ea5ebeed2237564fa4f3823
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-11-25 11:11:40 -0300

    Ajuste do merge

--- src/main.R
+++ src/main.R
@@ -1,36 +1,65 @@
-# library(tidyr)
-# library(dplyr)
-# library(dslabs)
-# library(rpart.plot)
-# library(tidyverse)  # data manipulation
-# # install.packages("tidyverse")
-# library(cluster)    # clustering algorithms
-# library(factoextra)
-# library(FactoMineR)
+library(tidyr)
+library(dplyr)
+library(dslabs)
+library(rpart.plot)
+library(tidyverse)  # data manipulation
+install.packages("tidyverse")
+library(cluster)    # clustering algorithms
+library(factoextra)
+library(FactoMineR)
 library(ggplot2)
 library(RColorBrewer)
-# install.packages("RColorBrewer")
+install.packages("RColorBrewer")
 library(ggpubr)
 install.packages("RColorBrewer")
 
 myPalette <- brewer.pal(5, "Set2") 
 
-data <- read.delim("./cardiovascular.txt", sep =";", na.strings = c('','NA','na','N/A','n/a','NaN','nan'), header = TRUE)
+data <- read.csv("./dataset.txt", sep =";", na.strings = c('','NA','na','N/A','n/a','NaN','nan'), header = TRUE)
 
-#  ------------------- Entendimento previo e limpeza do dataset ----------------------------------
+#  ------------------- Parte 1 - Entendimento previo e limpeza do dataset ----------------------------------
 View(head(data, n=5))
 dim(data) # 462 linhas e 11 colunas (sendo uma coluna identificador)
 str(data)
 
-# Verificar dados vazios em cada coluna e contabilidar
+# Verificar dados vazios em cada coluna e contabilizar
 View(sapply(data, function(x) sum(is.na(x))))
 sum(is.na(data))
 
 # retirar coluna de identificação
 data <- data[-1]
 
+comDoenca = sum(data$chd == 1) # 160 tiveram doença na coronaria
+semDoenca = sum(data$chd == 0) # 302 Não tiveram doença na coronaria
 
-# tranformando "Present" e "Absent" em dados númerico
+#Faixa de idade que tiveram doença coronaria tendo histórico familiar
+coronary = filter(data, chd == 1)
+nonCoronary = filter(data, chd == 0)
+
+# Faixa de idade do dataset
+ggplot(data, aes(x=age)) +  geom_histogram(color="black", fill="white", bins = 30)+ geom_density(alpha=.2, fill="#FF6666")
+
+
+ggplot(coronary, aes(x=age, fill = famhist)) +    geom_bar() +
+  scale_x_binned() + labs(title = "Plot de doença coronária") +
+  xlab("Idade") + ylab("Quantidade de pessoas") 
+
+# Explorando features - Doença coronaria
+boxplot(coronary$tobacco, coronary$obesity, coronary$ldl)
+
+doencaPlot <- pie(c(comDoenca, semDoenca), c("Doença Coronária","Sem doença Coronária"), border = "white", col=myPalette,
+                  main = "Distribuição do dataset")
+
+#Faixa de idade do dataset
+idadePlot <-ggplot(data, aes(x=age)) +  geom_histogram(color="black", fill="white")+ geom_density(alpha=.2, fill="#FF6666")
+
+
+histPlot <- ggplot(coronary, aes(x=age, fill = famhist)) +    geom_bar() +
+  scale_x_binned()  +
+  xlab("Idade") + ylab("Quantidade de pessoas") 
+
+ggarrange(idadePlot, histPlot, labels = c("Faixa de idade do dataset", "Doença coronaria e histórico familiar"), nrow = 2)
+# tranformando "Present" e "Absent" em dados númericos
 data$famhist <- replace(data$famhist, data$famhist == "Present", "1") # Atribuindo Present com 1
 data$famhist <- replace(data$famhist, data$famhist == "Absent", "2") # Atribuindo Absent com 2
 data$famhist <- as.numeric(data$famhist)
@@ -42,6 +71,9 @@ str(data)
 # Ao gerar o boxplot de de distribuição de dados por todas features 
 # vimos a necessidade de normalizar os dados.
 boxplot(data) 
+
+
+# ------------------------------------------2 parte - IA -----------------------------------------------------------
 
 chd_feature <- data[10]
 famhist_feature <- data[5]
@@ -72,63 +104,6 @@ dim(test)
 # preparando chd para colorir plot
 chd <- as.factor(train$chd)
 
-# ----------------- Exploração inicial dos dados ------------------------------
-comDoenca = sum(data$chd == 1) # 160 tiveram doença na coronaria
-semDoenca = sum(data$chd == 0) # 360 Não tiveram doença na coronaria
-
-famhist_present = sum(data$famhist == 1) # 160 tiveram doença na coronaria
-famhist_ausent = sum(data$famhist == 2) # 360 Não tiveram doença na coronaria
-
-# Faixa de idade do dataset
-ggplot(train, aes(x=age)) +  geom_histogram(color="black", fill="white", bins = 30)+ geom_density(alpha=.2, fill="#FF6666")
-
-# Faixa de idade que tiveram doença coronaria tendo histórico familiar
-coronary = filter(train, chd == 1)
-
-ggplot(coronary, aes(x=age, fill = famhist)) +    geom_bar() +
-  scale_x_binned() + labs(title = "Plot de doença coronária") +
-  xlab("Idade") + ylab("Quantidade de pessoas") 
-
-# Explorando features - Doença coronaria
-boxplot(coronary$tobacco, coronary$obesity, coronary$ldl)
-
-doencaPlot <- pie(c(comDoenca, semDoenca), c("Doença Coronária","Sem doença Coronária"), border = "white", col=myPalette,
-    main = "Distribuição do dataset")
-
-#Faixa de idade do dataset
-idadePlot <-ggplot(data, aes(x=age)) +  geom_histogram(color="black", fill="white")+ geom_density(alpha=.2, fill="#FF6666")
-
-
-#Faixa de idade que tiveram doença coronaria tendo histórico familiar
-coronary = filter(data, chd == 1)
-nonCoronary = filter(data, chd == 0)
-
-histPlot <- ggplot(coronary, aes(x=age, fill = famhist)) +    geom_bar() +
-  scale_x_binned()  +
-  xlab("Idade") + ylab("Quantidade de pessoas") 
-
-ggarrange(idadePlot, histPlot, labels = c("Faixa de idade do dataset", "Doença coronaria e histórico familiar"), nrow = 2)
-
-#Explorando features - Doença coronaria
-doencaFeaturesPlot <- boxplot(coronary$tobacco, coronary$obesity, coronary$ldl,
-
-        names = c("Acumulativa de tabacco (kg)", "Nível de obesidade", "Colesterol (LDL)"),
-        border = "brown",
-        main = "Doença coronária",
-        col = "orange")
-
-
-# Plot geral das features
-plot(train, col = chd)
-
-# É possivél ver um excesso de gordura no organismo ao longo da idade e também o aumento de numero 
-# de paciantes com resultado de doença coronária. 
-plot(x = train$age, y = train$adiposity, col = chd, pch =19, main="Adiposidade x Idade",
-     ylab="Adiposidade", xlab="Idade")
-
-
-pie(c(comDoenca, semDoenca), c("Doença Coronária","Sem doença Coronária"), border = "white", col=myPalette,
-    main = "Distribuição do dataset")
 
 # Gráfico de pizza - quantidade de pacientes com resposta de doença cardíaca coronária
 diagnostico_frequencia <-table(train$chd)
@@ -145,13 +120,3 @@ ggplot(diagnostico_tabela, aes(x="", y= Freq, fill=heart_attack))+
   geom_text(aes(y = c(66, 20), label = Freq), color = "white", size=6) +
   scale_fill_brewer(palette="Set1") +
   labs(x=NULL, y=NULL, title="Presença '1' e Ausencia '0' de crise cardiaca")
-#p <- ggplot(ToothGrowth, aes(x=dose, y=len)) + 
-#  geom_boxplot()
-
-
-NdoencaFeaturesPlot <- boxplot(nonCoronary$tobacco, nonCoronary$obesity, nonCoronary$ldl,
-                              names = c("Acumulativa de tabacco (kg)", "Nível de obesidade", "Colesterol (LDL)"),
-                              border = "brown",
-                              main = "Sem doença coronária",
-                              col = "orange")
-
